09a60b7d15204de2d5169185eb1ef21b4237e5dd
these9907 <these9907@moveint.io>
2025-12-31 10:45:19 +0900

    UI 업데이트

--- backend/uv/algorithms/data_structures/heap/heap.py
+++ backend/uv/algorithms/data_structures/heap/heap.py
@@ -52,3 +52,50 @@ class Heap[T: Comparable]:
             right_child = self.right_child_index(index)
             if left_child is not None and self.h[left_child] > self.h[violation]:
                 violation = left_child
+            if right_child is not None and self.h[right_child] > self.h[violation]:
+                violation = right_child
+            if violation != index:
+                self.h[violation], self.h[index] = self.h[index], self.h[violation]
+                self.max_heapify(violation)
+
+    def build_max_heap(self, collection: Iterable[T]) -> None:
+        self.h = list(collection)
+        self.heap_size = len(self.h)
+        if self.heap_size > 1:
+            for i in range(self.heap_size // 2 - 1, -1, -1) :
+                self.max_heapify(i)
+
+    def extract_max(self)  -> T:
+        if self.heap_size >= 2:
+            me = self.h[0]
+            self.h[0] = self.h.pop(-1)
+            self.heap_size -= 1
+            self.max_heapify(0)
+            return me
+        elif self.heap_size == 1:
+            self.heap_size -= 1
+            return self.h.pop(-1)
+        else:
+            raise Exception("Empty Heap")
+
+    def insert(self, value: T) -> None:
+        self.h.append(value)
+        idx = (self.heap_size - 1) // 2
+        self.heap_size += 1
+        while idx >= 0:
+            self.max_heapify(idx)
+            idx = (idx - 1) // 2
+
+    def heap_sort(self) -> None:
+        size = self.heap_size
+        for i in range(size - 1, 0 , -1):
+            self.h[0], self.h[i] = self.h[i], self.h[0]
+            self.heap_size -= 1
+            self.max_heapify(0)
+        self.heap_size = size
+
+
+
+if __name__ == "__main__":
+    lst = [1,2,3]
+    print(lst.pop(-1))
